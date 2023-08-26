79c01e603f796e43d5287abcd30bf2a09b46dd92
a55643215 <138569500+a55643215@users.noreply.github.com>
2023-08-26 11:17:17 +0800

    up

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ import os
 app = Flask(__name__)#admin: !QAZ2wsx資料庫的帳號和密碼
 #讓程式自己去判斷如果是測試端就會使用APP_SETTINGS
 
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
 if __name__ == '__main__':
