2390d85b611365dfbd1822ae5edd87d74fd2741d
a55643215 <138569500+a55643215@users.noreply.github.com>
2023-08-26 14:52:04 +0800

    up

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
+from models.user import Users
+from models.database import db_session,init_db
+from models.product import Products
 app = Flask(__name__)
 
 
@@ -16,6 +18,18 @@ handler = WebhookHandler('b3b067df5cd7d0d0496b8ed94a1dd873')
 
 
 app = Flask(__name__)
+
+#建立或取得user
+def get_or_create_user(user_id):
+    user = db_session.query(Users).filter_by(id=user_id).first()
+
+    if not user:
+        profile = line_bot_api.get_profile(user_id)
+
+        user = Users(id=user_id,nike_name=profile.display_name, image_url=profile.picture_url)
+        db_session.add(user)
+        db_session.commit()
+    return user
 
 
 def about_us_event(event):
@@ -70,16 +84,43 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #get_or_create_user(event.source.user_id)
+    get_or_create_user(event.source.user_id)
     profile = line_bot_api.get_profile(event.source.user_id)
     uid = profile.user_id
     message_text = str(event.message.text).lower()
 
     if message_text == '@使用說明':
         about_us_event
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text='HI! Welcome to LSTORE.')
+    elif message_text == '我想訂購商品':
+        message = Products.list_all()
+    if message:
+        line_bot_api.reply_message(
+        event.reply_token,
+        message
     )
-	
+#初始化產品資訊
+@app.before_first_request
+def init_products():
+    # init db
+    result = init_db()#先判斷資料庫有沒有建立，如果還沒建立就會進行下面的動作初始化產品
+    if result:
+        init_data = [Products(name='Coffee',
+                              product_image_url='https://i.imgur.com/DKzbk3l.jpg',
+                              price=150,
+                              description='nascetur ridiculus mus. Donec quam felis, ultricies'),
+                     Products(name='Tea',
+                              product_image_url='https://i.imgur.com/PRTxyhq.jpg',
+                              price=120,
+                              description='adipiscing elit. Aenean commodo ligula eget dolor'),
+                     Products(name='Cake',
+                              price=180,
+                              product_image_url='https://i.imgur.com/PRm22i8.jpg',
+                              description='Aenean massa. Cum sociis natoque penatibus')]
+        db_session.bulk_save_objects(init_data)#透過這個方法一次儲存list中的產品
+        db_session.commit()#最後commit()才會存進資料庫
+        #記得要from models.product import Products在app.py
+        
+
 if __name__ == "__main__":
+    init_products()
     app.run()
---
+++ models/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from collections.abc import Iterable
+from sqlalchemy_utils import database_exists
+
+import os
+
+currend_dir = os.path.dirname(__file__)
+db_path = r'sqlite:///{}/lstore.db'.format(currend_dir)
+engine = create_engine(db_path, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    if database_exists(db_path):
+        return False
+    else:
+        Base.metadata.create_all(engine)
+        return True
