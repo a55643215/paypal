c81ada749355aae2f075d2ee03b6a77249799c2c
david <a55643215@gmail.com>
2023-09-02 13:15:10 +0800

    up

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ class Config:
     CHANNEL_ACCESS_TOKEN ='N44yZEfmA3bq6c/FGbOu2CfLZ13p0sO9nFcSuymeArXSc7uF8qOIhA6eZaATEu1K9RCVkV+6GmJBJouIH2NotgsPXjCvUwsgAKd5pATaHUtlaR0hg7R8+IzpJ7nsl1gcZW914FKo67sd5246tqrG8gdB04t89/1O/w1cDnyilFU='
     CHANNEL_SECRET='b3b067df5cd7d0d0496b8ed94a1dd873'
     
+
+    LINE_PAY_ID = '2000604811'
+    LINE_PAY_SECRET = '85c2256b73ef51f3adfcd8bc189172f5'
+
+    STORE_IMAGE_URL = 'https://i.imgur.com/fCreVgv.gifv'
